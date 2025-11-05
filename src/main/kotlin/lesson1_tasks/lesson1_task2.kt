7db426cb757ab71c40a15e23f25dd74c4e0e4b40
Stas <sinegub.stas12@gmail.com>
2025-11-05 21:54:27 +0300

    KS-1-2 added workers

---
+++ src/main/kotlin/lesson1_tasks/lesson1_task2.kt
@@ -0,0 +1,17 @@
+package org.example.lesson1_tasks
+
+fun main() {
+
+    val numberOfOrders: Int = 75
+    val welcomeMessage: String = "Thank you for your order"
+    val numberOfWorkersOfTheOnlineStore: String = "Number of workers of the online store:"
+    var numberOfWorkers: Short = 2000
+
+    println(numberOfOrders)
+    println(welcomeMessage)
+    println(numberOfWorkersOfTheOnlineStore)
+//    println(numberOfWorkers)
+
+    numberOfWorkers = 1999
+    println(numberOfWorkers)
+}
