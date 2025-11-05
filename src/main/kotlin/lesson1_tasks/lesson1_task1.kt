1758e90bcf83bfd3cf8345a25d860f9cc1037bb0
Stas <sinegub.stas12@gmail.com>
2025-11-05 18:03:10 +0300

    KSOL-1-1 added variables

---
+++ src/main/kotlin/lesson1_tasks/lesson1_task1.kt
@@ -0,0 +1,9 @@
+package org.example.lesson1_tasks
+fun main () {
+
+    var numberOfOrders: Int = 75
+    val welcomeMessage: String = "Thank you for your order"
+
+    println(numberOfOrders)
+    println(welcomeMessage)
+}
