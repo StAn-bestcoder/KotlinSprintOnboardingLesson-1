872310701ccde12e2d98fdaa45acb358340bed5c
Stas <sinegub.stas12@gmail.com>
2025-11-05 21:09:30 +0300

    KSOL-1-1 variables mistake

--- src/main/kotlin/lesson1_tasks/lesson1_task1.kt
+++ src/main/kotlin/lesson1_tasks/lesson1_task1.kt
@@ -2,7 +2,7 @@ package org.example.lesson1_tasks
 
 fun main() {
 
-    var numberOfOrders: Int = 75
+    val numberOfOrders: Int = 75
     val welcomeMessage: String = "Thank you for your order"
 
     println(numberOfOrders)
