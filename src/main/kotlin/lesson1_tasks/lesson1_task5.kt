9a7726fa41bde48eb6567ffb8bd9a9d85a09ab34
Stas <sinegub.stas12@gmail.com>
2025-11-06 17:57:47 +0300

    KS-1-5 flight time

---
+++ src/main/kotlin/lesson1_tasks/lesson1_task5.kt
@@ -0,0 +1,15 @@
+package org.example.lesson1_tasks
+
+fun main() {
+    val hour: String = "01"
+    val colonFirst: Char = ':'
+    val minutes: Byte = 48
+    val colonSecond: Char = ':'
+    val seconds: String = "00"
+
+    print(hour)
+    print(colonFirst)
+    print(minutes)
+    print(colonSecond)
+    print(seconds)
+}
