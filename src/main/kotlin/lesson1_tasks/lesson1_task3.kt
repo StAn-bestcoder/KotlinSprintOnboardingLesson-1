b56a41a9684fbc34cf46e5a27a0b8a377316dbbe
Stas <sinegub.stas12@gmail.com>
2025-11-06 16:41:02 +0300

    KS-1-3 space flight

---
+++ src/main/kotlin/lesson1_tasks/lesson1_task3.kt
@@ -0,0 +1,18 @@
+package org.example.lesson1_tasks
+
+fun main() {
+    val year: Short = 1961
+    var hour: Byte = 9
+    val colon: Char = ':'
+    var minute: Byte = 7
+
+    println(year)
+//    print(hour)
+//    print(minute)
+
+    hour = 10
+    minute = 55
+    print(hour)
+    print(colon)
+    print(minute)
+}
